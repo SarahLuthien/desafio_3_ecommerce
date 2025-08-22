45c3801f7a36626b8fafd2ffb617fde81afd9b6c
SarahLuthien <melian.maia.m@gmail.com>
2025-08-22 19:20:07 -0300

    chore: Configure Backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -21,13 +21,12 @@ import { Category } from './categories/entities/category.entity';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DB_HOST', 'database'),
-        port: configService.get<number>('DB_PORT', 5432),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
+        url: configService.get<string>('DATABASE_URL'),
         entities: [Product, Category],
         synchronize: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       }),
     }),
 
